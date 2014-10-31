a0f1e21d3b1d1de6a1f299b6a41a74bc9fd772a9
John Turingan <john.turingan@gmail.com>
2014-10-31 15:05:36 +0800

    Added options

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var NAME = 'gulp-markdown-to-json';
+var NAME = 'gulp-html-to-json';
 var fs      = require("fs"),
     path    = require("path"),
     es      = require("event-stream"),
-    gutil   = require("gulp-util");
+    gutil   = require("gulp-util"),
+    marked = require('marked'),
+    frontmatter = require('front-matter');
 
-DIRECTIVE_REGEX = /^(.*=\s*([\w\.\/-]+)\s*:\s*([\w\.\/-]+\.html?\s*))$/gm;
+
+DIRECTIVE_REGEX = /^(.*=\s*([\w\.\*\/-]+)\s*:\s*([\w\.\/-]+\.html?\s*))$/gm;
 IS_HIDDEN_REGEX = /(^|.\/)\.+[^\/\.]/g;
 
 
@@ -25,6 +28,7 @@ function getFiles(dir, cb){
     }
 }
 
+
 function matchExtension(extension, params) {
     if (params.extensions) {
         if (Array.isArray(params.extensions)) {
@@ -40,9 +44,40 @@ function matchExtension(extension, params) {
     return false;
 }
 
+function parse(file){
+
+    if (fs.existsSync(file)) {
+
+        var bufferContents = fs.readFileSync(file);
+        parsed = frontmatter(bufferContents.toString().replace(/\s+/g, ' '));
+
+        return parsed.body;
+
+    } else {
+        throw new gutil.PluginError('gulp-html-to-json', 'File not found: ' + fullPath);
+    }
+
+}
+
+function indName (dirname, splitter) {
+
+    splitter = splitter || '/';
+    var n = dirname.split('.');
+    var nArr = n.pop();
+    nArr = n.pop().split(splitter);
+
+    return nArr.pop();
+}
+
+function replaceFilename (path, fname) {
+    var filename = path.replace(/^.*(\\|\/|\:)/, '');
+    var nFname = path.replace(filename, fname);
+     return gutil.replaceExtension(nFname, '.json');
+}
+
+
 
 module.exports = function(params) {
-
     var params = params || {};
     function include(file, callback) {
 
@@ -59,24 +94,41 @@ module.exports = function(params) {
             var text = String(file.contents);
             var newText = text;
             var matches;
+            var outputJson = {};
 
             while (matches = DIRECTIVE_REGEX.exec(text)) {
                 var match       = matches[1],
-                    relPath     = path.dirname( file.path ),
+                    relPath     = path.dirname(file.path ),
                     fullPath    = path.join(relPath,  matches[3].replace(/['"]/g, '')).trim(),
                     jsonVar     = matches[2],
+                    body        = "",
                     extension   = matches[3].split('.').pop();
 
-                    if (fs.existsSync(fullPath)) {
-                        console.log("exist");
-                    } else {
-                        throw new gutil.PluginError('gulp-html-to-json', 'File not found: ' + fullPath);
-                    }
+                var _inc = parse(fullPath);
+
+                if (_inc.length > 0) {
+                    var ind = (jsonVar.trim() == '*') ? indName(matches[3]) : jsonVar
+
+                    outputJson[ind] = _inc;
+                }
             }
+
+            params.filename || (params.filename = indName(file.path, "\\"));
+            params.useAsVariable || (params.useAsVariable = false);
+
+
+
+            //file.path = gutil.replaceExtension(file.path, '.json');
+            file.path = replaceFilename(file.path, params.filename)
+
+            var exVars = (params.useAsVariable) ? "var " + params.filename + "=" : ""
+            file.contents = new Buffer(exVars + JSON.stringify(outputJson));
         }
 
         callback(null, file);
     }
+
+
 
     return es.map(include)
 }
